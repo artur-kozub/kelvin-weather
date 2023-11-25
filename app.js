b236808a68a64976aecd1663ffe852fec5076594
Arthur Kozub <kozub0246@gmail.com>
2023-11-25 22:57:35 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// the variable kevin equals to 293 Kelvin
+const kelvin = 0;
+// have calculated the value of Kelvins but in Clesius
+const celsius = kelvin - 273;
+// in the next line we calculate the value of Celsius in Fahrenheits
+let fahrenheit = celsius * (9/5) + 32;
+// in the string below we get the value of fahrenheit to be rounded
+fahrenheit = Math.floor(fahrenheit);
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
